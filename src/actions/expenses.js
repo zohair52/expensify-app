197e67e39bcde0bf852830a782284ef8520fa067
Zohair Hussain <zohair.hussain@valuemomentum.com>
2020-04-23 08:26:27 -0400

    Initial commit

---
+++ src/actions/expenses.js
@@ -0,0 +1,34 @@
+import uuid from 'uuid';
+//Add_expense
+export const addExpense = (
+    {
+        description = '', 
+        note='',
+         amount=0, 
+         createdAt=0
+    } = {}
+         
+)   => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+//Remove Expense
+export const removeExpense=({id}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+    
+});
+
+//Edit Expense
+export const EditExpense= (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
---
+++ src/actions/filters.js
@@ -0,0 +1,30 @@
+
+
+
+//set Text filer
+export const setTextFilter = (text = '') =>({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+//set sortBy amount
+export const sortByAmount = (sortBy = '') => ({
+    type: 'SORT_BY_AMOUNT',
+});
+
+// sort by date
+export const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+//set startDate
+export const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+//set endDate
+export const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
