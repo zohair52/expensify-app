5b6147af9f5810d6a39f52184d2438024292e397
Zohair Hussain <zohair.hussain@valuemomentum.com>
2020-04-28 22:18:14 -0400

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -66,20 +66,22 @@ export default class ExpenseForm extends React.Component {
     };
     render() {
         return (
-            <div>
-            {this.state.error && <p>{this.state.error}</p>}
 
-                <form onSubmit= {this.onSubmit}>
+                <form className="form" onSubmit= {this.onSubmit}>
+                    {this.state.error && <p classNam="form__error">{this.state.error}</p>}
+
                     <input 
                         type ="text"
                         placeholder= "Description"
                         autoFocus
+                        className = "text-input"
                         value= {this.state.description}
                         onChange= {this.onDescriptionChange}
                     />
                     <input
                         type="number"
                         placeholder="Amount"
+                        className = "text-input"
                         value= {this.state.amount}
                         onChange={this.onAmountChange}
                     />
@@ -93,16 +95,18 @@ export default class ExpenseForm extends React.Component {
                         />
                     <textarea
                         placeholder= "Add a note for your expense (optional)"
+                        className= "textarea"
                         value= {this.state.note}
                         onChange= {this.onNoteChange}
                     >
                         
                     </textarea>
-                    <button>Add Expense</button>
+                    <div>
+                     <button className= "button">Save Expense</button>
+                    </div>
                 </form>
 
 
-            </div>
         )
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink,  Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {startLogout, startLogin} from '../actions/auth';
 
 export const Header = ({startLogout}) => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    <button onClick={startLogout}> Logout</button>
-    </header>
+  <header className="header">
+    <div className="content-container">
+      <div className= "header__content">
+        <Link className="header__title" to="/dashboard" >
+          <h1>Expensify</h1>
+        </Link>
+        
+        <button className="button button--link" onClick={startLogout}> Logout</button>
+      </div>
+    </div>
+  </header>
+
 );
 
 const mapDipatchToProps = (dispatch) => ({
